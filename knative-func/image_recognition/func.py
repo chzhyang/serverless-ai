a410bf451869af32b791a7e4ab32124d19cdd2f7
chzhyang <ge.yang@intel.com>
2022-11-09 22:22:21 -0800

    move functions to image recognition service class

--- knative-func/image_recognition/func.py
+++ knative-func/image_recognition/func.py
@@ -1,34 +1,38 @@
+import os
 import time
+from urllib import request
 from parliament import Context
 from flask import Request, jsonify
+from pathlib import Path
+import image_recognition_service
 
-import utils
+DATA_DIR = Path(__file__).resolve().parent / 'data'
+TEST_IMAGE = os.path.join(DATA_DIR, 'test.JPEG')
+MODEL_PATH = os.path.join(DATA_DIR, 'resnet50_fp32_pretrained_model.pb')
+LABELS_PATH = Path(__file__).resolve().parent / 'data'/ 'labellist.json'
+SERVICE = image_recognition_service.ImageRecognitionService(MODEL_PATH, LABELS_PATH)
 
-RESNET_IMAGE_SIZE = 224
-TEST_IMAGE = './data/test.JPEG'
-MODEL_PATH = 'models/resnet50_fp32_pretrained_model.pb'
+def download_image(img_url, img_dir):
+  """Download image from URL to default filepath"""
+  if not os.path.exists(img_dir):
+      os.makedirs(img_dir)
+  img_name = img_url.split('/')[-1].split('.')[0]+'.jpg'
+  img_filepath = os.path.join(img_dir, img_name)
+  if not os.path.exists(img_filepath):
+    img_data = request.get(img_url)
+    with open(img_filepath, 'wb') as f:
+      f.write(img_data.content)
+    print("Download image to ", img_filepath, flush=True)
+  else:
+    print("Image exists: ", img_filepath)
+  return img_filepath
 
-utils.optimized_config()
-INFER_GRAPH, INFER_SESS = utils.load_model(MODEL_PATH)
-
-# """Run standard ImageNet preprocessing on image file.
-  # Args:
-  #   file_name: string, path to file containing a JPEG image
-  #   output_height: int, final height of image
-  #   output_width: int, final width of image
-  #   num_channels: int, depth of input image
-  # Returns:
-  #   Float array representing processed image with shape
-  #     [output_height, output_width, num_channels]
-  # Raises:
-  #   ValueError: if image is not a JPEG.
-  # """
-def request_handler(req: Request, infer_graph, infer_sess) -> str:
+def request_handler(req: Request, svc) -> str:
   """Handle the request"""
   if req.method == "GET":
     start_time = time.time()
-    predictions, data_time, infer_time= utils.run_inference(TEST_IMAGE, infer_graph, infer_sess)
-    predictions_lables = utils.get_top_predictions(predictions, False, 5)
+    predictions, data_time, infer_time= svc.run_inference(TEST_IMAGE)
+    predictions_lables = svc.get_top_predictions(predictions, False, 5)
     total_time = time.time()-start_time
     result = {
       "top5_predictions": predictions_lables, 
@@ -42,10 +46,10 @@ def request_handler(req: Request, infer_graph, infer_sess) -> str:
     start_time = time.time()
     data = req.get_json()
     img_url = data["imgURL"]
-    img_filepath = utils.download_image(img_url)
-    predictions, data_time, infer_time = utils.run_inference(img_filepath, infer_graph, infer_sess)
+    img_filepath = download_image(img_url, DATA_DIR)
+    predictions, data_time, infer_time = svc.run_inference(img_filepath)
     total_time = time.time()-start_time
-    predictions_lables = utils.get_top_predictions(predictions, False, 5)
+    predictions_lables = svc.get_top_predictions(predictions, False, 5)
     result = {
       "top5_predictions": predictions_lables, 
       "data_latency(ms)": data_time,
@@ -60,21 +64,7 @@ def main(context: Context):
   Image classifier with optimized TensorFlow graph
   """
   if 'request' in context.keys():
-    return request_handler(context.request, INFER_GRAPH, INFER_SESS)
+    return request_handler(context.request, SERVICE)
   else:
-    # performance test
-    img_url = "https://raw.githubusercontent.com/chzhyang/faas-workloads/main/tensorflow/image_recognition/tensorflow_image_classification/data/ILSVRC2012_test_00000181.JPEG"
-    img_filepath = utils.download_image(img_url)
-    predictions, latency = utils.run_inference(img_filepath, INFER_GRAPH, INFER_SESS)
-    predictions_lables = utils.get_top_predictions(predictions, False, 5)
-    result = {
-      "top5_predictions" : predictions_lables, 
-      "inference_latency(ms)" : latency
-    }
-    print(result, flush=True)
-    # headers = { "content-type": "application/json" }
-    return jsonify(result)
-    # test end
-    # print("Empty request", flush=True)
-    # print("Empty request", flush=True)
-    # return "{}", 400
+    print("Empty request", flush=True)
+    return "{}", 400
---
+++ knative-func/image_recognition/image_recognition_service.py
@@ -0,0 +1,121 @@
+import imghdr
+import json
+import os
+from pathlib import Path
+import subprocess
+import time
+
+import imagenet_preprocessing
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.framework import dtypes
+from tensorflow.python.tools.optimize_for_inference_lib import \
+    optimize_for_inference
+
+RESNET_IMAGE_SIZE = 224
+INPUTS = 'input'
+OUTPUTS = 'predict'
+
+class ImageRecognitionService():
+  def __init__(self, model_path, lables_path):
+    self._optimized_config()
+    self.model_path = model_path
+    self.lables_path = lables_path
+    self.infer_graph, self.infer_sess = self._load_model()
+    self.input_tensor = self.infer_graph.get_tensor_by_name('input:0')
+    self.output_tensor = self.infer_graph.get_tensor_by_name('predict:0')
+    self._cache_model()
+    print("Ready for inference...", flush=True)
+
+  def _optimized_config(self):
+    # Get all physical cores
+    num_physical_cores = subprocess.getoutput('lscpu -b -p=Core,Socket | grep -v \'^#\' | sort -u | wc -l')
+    os.environ["KMP_BLOCKTIME"] = "1"
+    os.environ["KMP_SETTINGS"] = "1"
+    os.environ["KMP_AFFINITY"]= "granularity=fine,verbose,compact,1,0"
+    os.environ["OMP_NUM_THREADS"]= num_physical_cores
+    
+    tf.config.threading.set_inter_op_parallelism_threads(1)
+    tf.config.threading.set_intra_op_parallelism_threads(int(num_physical_cores))
+
+  def _load_model(self):
+    # Load model
+    print("Load model...", flush=True)
+    infer_graph = tf.Graph()
+    with infer_graph.as_default():
+      graph_def = tf.compat.v1.GraphDef()
+      with tf.compat.v1.gfile.FastGFile(self.model_path, 'rb') as input_file:
+        input_graph_content = input_file.read()
+        graph_def.ParseFromString(input_graph_content)
+        output_graph = optimize_for_inference(graph_def, [INPUTS], [OUTPUTS], dtypes.float32.as_datatype_enum, False)
+      tf.import_graph_def(output_graph, name='')
+      infer_sess = tf.compat.v1.Session(graph=infer_graph)
+    return infer_graph, infer_sess
+    
+  def _cache_model(self):
+    # Use random data to cache model
+    print("Cache model...", flush=True)
+    data_graph = tf.Graph()
+    with data_graph.as_default():
+      input_shape = [1, RESNET_IMAGE_SIZE, RESNET_IMAGE_SIZE, 3]
+      images = tf.random.uniform(input_shape, 0.0, 255.0, dtype=tf.float32, name='synthetic_images')
+    data_sess = tf.compat.v1.Session(graph=data_graph)
+    image_np = data_sess.run(images)
+    self.infer_sess.run(self.output_tensor, feed_dict={self.input_tensor: image_np})
+
+  def _data_preprocess(self, data_location, batch_size, output_height, output_width, num_channels=3):
+    data_graph = tf.Graph()
+    with data_graph.as_default():
+      if imghdr.what(data_location) != "jpeg":
+            raise ValueError("At this time, only JPEG images are supported, please try another image.")
+      image_buffer = tf.io.read_file(data_location)
+      image_array = imagenet_preprocessing.preprocess_image(image_buffer,None,output_height,output_width,num_channels,False)
+      input_shape = [batch_size, output_height, output_width, num_channels]
+      images = tf.reshape(image_array, input_shape)
+    data_sess = tf.compat.v1.Session(graph=data_graph)
+    image_np = data_sess.run(images)
+
+    return image_np
+
+  def run_inference(self, data_location):
+    """Run inference"""
+    start_time = time.time()
+    image_np = self._data_preprocess(data_location, 1, RESNET_IMAGE_SIZE, RESNET_IMAGE_SIZE, 3)
+    data_time = time.time() - start_time
+
+    start_time = time.time()
+    predictions = self.infer_sess.run(self.output_tensor, feed_dict={self.input_tensor: image_np})
+    infer_time = (time.time() - start_time)
+
+    return predictions, data_time * 1000, infer_time * 1000
+
+  def _get_labels(self):
+    """Get the set of possible labels for classification"""
+    with open(self.lables_path, "r") as labels_file:
+      labels = json.load(labels_file)
+
+    return labels
+
+  def _top_predictions(self, result, n):
+    """Get the top n predictions given the array of softmax results"""
+    # Only care about the first example.
+    probabilities = result
+    # Get the ids of most probable labels. Reverse order to get greatest first.
+    ids = np.argsort(probabilities)[::-1]
+    
+    return ids[:n]
+
+  def _get_labels_for_ids(self, labels, ids, ids_are_one_indexed=False):
+    """Get the human-readable labels for given ids"""
+    return [labels[str(x + int(ids_are_one_indexed))] for x in ids]
+
+  def get_top_predictions(self, results, ids_are_one_indexed=False, preds_to_print=5):
+    """Given an array of mode, graph_name, predicted_ID, print labels"""
+    labels = self._get_labels()
+    predictions = []
+    for result in results:
+      pred_ids = self._top_predictions(result, preds_to_print)
+      pred_labels = self._get_labels_for_ids(labels, pred_ids, ids_are_one_indexed)
+      predictions.append(pred_labels)
+
+    return predictions
