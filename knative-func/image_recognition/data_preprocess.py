e9965c72fe3289443a2f97d1a662782cdc42efa9
chzhyang <ge.yang@intel.com>
2022-11-14 22:20:25 -0800

    mv data preprocess to image recognition

--- knative-func/image_recognition/data_preprocess.py
+++ knative-func/image_recognition/data_preprocess.py
@@ -1,3 +1,4 @@
+import imghdr
 import tensorflow as tf
 
 _R_MEAN = 123.68
@@ -5,6 +6,7 @@ _G_MEAN = 116.78
 _B_MEAN = 103.94
 CHANNEL_MEANS = [_R_MEAN, _G_MEAN, _B_MEAN]
 RESIZE_MIN = 256
+
 
 def _central_crop(image, crop_height, crop_width):
     """Performs central crops of the given image list"""
@@ -17,6 +19,7 @@ def _central_crop(image, crop_height, crop_width):
     crop_left = amount_to_be_cropped_w // 2
     return tf.slice(image, [crop_top, crop_left, 0], [crop_height, crop_width, -1])
 
+
 def _mean_image_subtraction(image, means, num_channels):
     """Subtracts the given means from each image channel"""
     if image.get_shape().ndims != 3:
@@ -28,6 +31,7 @@ def _mean_image_subtraction(image, means, num_channels):
     means = tf.broadcast_to(means, tf.shape(image))
 
     return image - means
+
 
 def _smallest_size_at_least(height, width, resize_min):
     """Computes new shape with the smallest side equal to smallest_side"""
@@ -45,21 +49,37 @@ def _smallest_size_at_least(height, width, resize_min):
 
     return new_height, new_width
 
+
 def _image_resize(image, resize_min):
     """Resize images preserving the original aspect ratio"""
     shape = tf.shape(input=image)
     height, width = shape[0], shape[1]
     new_height, new_width = _smallest_size_at_least(height, width, resize_min)
     # Simple wrapper around tf.resize_images
-    resized_image = tf.compat.v1.image.resize(image, [new_height, new_width], method=tf.image.ResizeMethod.BILINEAR, align_corners=False)
+    resized_image = tf.compat.v1.image.resize(
+        image, [new_height, new_width], method=tf.image.ResizeMethod.BILINEAR, align_corners=False)
 
     return resized_image
 
-def image_preprocess(image_buffer, output_height, output_width, num_channels):
+
+def image_preprocess(data_location, output_height, output_width, num_channels, batch_size):
     """Preprocesses the given image, includes decoding, cropping, and resizing"""
+    data_graph = tf.Graph()
+    with data_graph.as_default():
+        if imghdr.what(data_location) != "jpeg":
+            raise ValueError(
+                "At this time, only JPEG images are supported, please try another image.")
+        image_buffer = tf.io.read_file(data_location)
+    data_sess = tf.compat.v1.Session(graph=data_graph)
+    image = data_sess.run(images)
+
     image = tf.image.decode_jpeg(image_buffer, channels=num_channels)
     image = _image_resize(image, RESIZE_MIN)
     image = _central_crop(image, output_height, output_width)
     image.set_shape([output_height, output_width, num_channels])
     image = _mean_image_subtraction(image, CHANNEL_MEANS, num_channels)
-    return image
+
+    input_shape = [batch_size, output_height,
+                   output_width, num_channels]
+    images = tf.reshape(image, input_shape)
+    return images
