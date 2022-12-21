cc99df2cd34fb67a8a0c051bee898d524ecb728e
chzhyang <ge.yang@intel.com>
2022-12-21 08:33:10 +0000

    develop s3 connection and operation in function

--- tf-knative-event-ceph/func.py
+++ tf-knative-event-ceph/func.py
@@ -1,11 +1,9 @@
 import os
 import json
 from pathlib import Path
-import requests
 import image_recognition_service
 from parliament import Context
 import boto3
-import ceph_s3
 
 PRETRAINED_MODEL = "resnet50_v1_5_fp32.pb"
 DATA_DIR = Path(__file__).resolve().parent / 'data'
@@ -16,7 +14,7 @@ LABELS_PATH = os.path.join(DATA_DIR, 'labellist.json')
 TEST_IMAGE = os.path.join(DATA_DIR, 'test.JPEG')
 
 # Number of top human-readable predictions
-NUM_TOP_PREDICTIONS = 5
+NUM_TOP_PREDICTIONS = 1
 
 # Init the image recognition service class
 SERVICE = image_recognition_service.ImageRecognitionService(MODEL_PATH)
@@ -30,7 +28,12 @@ ACCESS_KEY = "Y51LDIT65ZN41VVLKG0H"
 SECRET_KEY = "GOxbWKx5NunhlAt3xTvDUh3uHP04A6Cv3UFEwdGS"
 ENDPOINT_URL = "http://rook-ceph-rgw-my-store:80"
 
-S3 = ceph_s3.CephS3(ENDPOINT_URL, ACCESS_KEY, SECRET_KEY)
+S3 = boto3.client(
+    's3',
+    endpoint_url=ENDPOINT_URL,
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY)
+
 
 def main(context: Context):
     """
@@ -38,20 +41,26 @@ def main(context: Context):
     """
     if context.cloud_event.data != None:
         data_json = context.cloud_event.data
+        print(data_json, flush=True)
         data_dict = json.loads(data_json)
-        if data_dict["awsRegion"] == AWS_REGION and data_dict["eventName"] = EVENT_NAME:
+        if data_dict["awsRegion"] == AWS_REGION and data_dict["eventName"] == EVENT_NAME:
             object_key = data_dict["s3"]["object"]["key"]
-            # TODO: get object from ceph
-            file_bytes = S3.download_s3_file(BUCKET_NAME, object_key)
-            print(file_bytes.getvalue())
-            # TODO: inference from bytes not file
-            predictions = svc.run_inference(
-                img_filepath, LABELS_PATH, NUM_TOP_PREDICTIONS)
-            result = {
-                "top_predictions": predictions
-            }
-            print(result, flush=True)
-            return json.dumps(result), 200
+            print("Object key: ", object_key)
+            file_path = Path(__file__).resolve().parent / 'data' / object_key
+            if not os.path.exists(file_path):
+                print("Download file from s3 bucket", flush=True)
+                S3.download_file(BUCKET_NAME, object_key, file_path)
+                if not os.path.exists(file_path):
+                    print("Failed to download ", file_path)
+                else:
+                    print("Inference ", file_path, flush=True)
+                    predictions = SERVICE.run_inference(
+                        file_path, LABELS_PATH, NUM_TOP_PREDICTIONS)
+                    result = {
+                        "top_predictions": predictions
+                    }
+                    print(result, flush=True)
+                    return json.dumps(result), 200
     else:
         print("Empty event", flush=True)
-        return "{}", 200
+        return "{}", 400
