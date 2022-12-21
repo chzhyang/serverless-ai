fa2133453710a23b1db68347109faf6db9d5f242
chzhyang <ge.yang@intel.com>
2022-12-21 08:09:55 +0000

    test  image

--- tf-knative-event-ceph/test/test_s3_boto3_image.py
+++ tf-knative-event-ceph/test/test_s3_boto3_image.py
@@ -21,21 +21,17 @@ def main():
         aws_access_key_id=ACCESS_KEY,
         aws_secret_access_key=SECRET_KEY)
 
-    print("download object")
-    object_key = 'test3.txt'
-    file_name = Path(__file__).resolve().parent / 'test3.txt'
-    s3.download_file(BUCKET_NAME, object_key, file_name)
-    if os.path.exists(file_name):
-        with open(file_name, 'r') as f:
-            for line in f:
-                print(line)
-    else:
-        print(file_name, " not exist.")
-
-    print("upload file")
-    up_file_path = Path(__file__).resolve().parent / 'test4.txt'
-    response = s3.upload_file(up_file_path, BUCKET_NAME, 'test4.txt')
+    print("upload image")
+    up_file_path = Path(__file__).resolve().parent / 'test_img.JPEG'
+    response = s3.upload_file(up_file_path, BUCKET_NAME, 'test_img.JPEG')
     print(response)
+
+    print("download image")
+    object_key = 'test_img.JPEG'
+    file_name = Path(__file__).resolve().parent / 'download_img.JPEG'
+    s3.download_file(BUCKET_NAME, object_key, file_name)
+    if not os.path.exists(file_name):
+        print(file_name, " not exist.")
 
     print("list object")
     response = s3.list_objects(Bucket=BUCKET_NAME)
