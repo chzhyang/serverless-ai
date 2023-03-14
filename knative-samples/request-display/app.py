ef733a3afe2e344fdd6865fdff56680d774872cc
chzhyang <ge.yang@intel.com>
2023-03-14 03:28:45 +0000

    add request display func, support get, post and cloudevents

--- knative-samples/request-display/app.py
+++ knative-samples/request-display/app.py
@@ -16,22 +16,26 @@ def main(context: Context):
     # display cloudevent
     if context.cloud_event is not None:
         dict_data = context.cloud_event.data
+        resp = "Reveived a cloudevents.Event"
         log.info(
-            f'Reveived cloudevents.Event {dict_data["id"]} from {dict_data["source"]}')
+            f'Reveived cloudevents.Event from {dict_data["eventSource"]}, event id is {dict_data["eventId"]}')
         return json.dumps(resp), 200
 
     # display http request
     elif context.request is not None:
         if context.request.method == "GET":
-            log.info(f'Received a GET request')
-            return json.dumps("hello world")
+            resp = f'Received a GET request'
+            log.info(resp)
+            return json.dumps(resp)
         elif context.request.method == "POST":
-            log.info(f'Received a POST request: {context.request.get_data()}')
-            return json.dumps("hello world")
+            resp = f'Received a POST request'
+            log.info(
+                f'Received a POST request: {context.request.get_data(as_text=True)}')
+            return json.dumps(resp), 200
         else:
             resp = "Server just supports GET, POST and CloudEvent(POST) requeset now"
             log.error(resp)
-            return json.dumps(resp), 200
+            return json.dumps(resp), 400
     else:
         log.error("Empty request")
         return "{Empty request}", 400
