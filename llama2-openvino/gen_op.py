8e79486a19e7782f0e3e3ddabcbf8185d6390402
chzhyang <chzhyangchn@gmail.com>
2023-11-05 22:52:59 -0800

    add llama2 inference with openvino

---
+++ llama2-openvino/gen_op.py
@@ -0,0 +1,108 @@
+from transformers import LlamaTokenizer
+from optimum.intel.openvino import OVModelForCausalLM
+import time
+import argparse
+import logging as log
+import sys
+log.basicConfig(format='[ %(levelname)s ] %(message)s', level=log.INFO, stream=sys.stdout)
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument('-h',
+                    '--help',
+                    action='help',
+                    help='Show this help message and exit.')
+parser.add_argument('-m',
+                    '--model_id',
+                    required=True,
+                    type=str,
+                    help='Required. hugging face model id or local model path')
+parser.add_argument('-p',
+                    '--prompt',
+                    default='What is AI?',
+                    type=str,
+                    help='Required. prompt sentence')
+parser.add_argument('-l',
+                    '--max_sequence_length',
+                    default=128,
+                    required=False,
+                    type=int,
+                    help='maximun lengh of output')
+parser.add_argument('-d',
+                    '--device',
+                    default='CPU',
+                    required=False,
+                    type=str,
+                    help='device for inference')
+parser.add_argument('-a',
+                    '--accuracy-mode',
+                    default=False,
+                    required=False,
+                    type=bool,
+                    help='')
+args = parser.parse_args()
+
+log.info(" --- load tokenizer --- ")
+tokenizer = LlamaTokenizer.from_pretrained(args.model_id, trust_remote_code=True)
+
+# config
+# config execution model hint(inference_precision)
+import openvino as ov
+core = ov.Core()
+if args.accuracy_mode:
+    print(" --- set CPU execution hint --- ")
+    core.set_property(
+        "CPU",
+        {ov.properties.hint.execution_mode(): ov.properties.hint.ExecutionMode.ACCURACY},
+    )
+# config ov
+# ov_config = {'PERFORMANCE_HINT': 'LATENCY', 'NUM_STREAMS': '1', "CACHE_DIR": ""}
+
+# load model
+try:
+    log.info(" --- use local model --- ")
+    model = OVModelForCausalLM.from_pretrained(args.model_id, compile=False, device=args.device)
+    # model = OVModelForCausalLM.from_pretrained(args.model_id, compile=False, device=args.device, ov_config=ov_config)
+except:
+    log.info(" --- use remote model --- ")
+    model = OVModelForCausalLM.from_pretrained(args.model_id, compile=False, device=args.device, export=True)
+model.compile()
+
+# get inference precision
+inference_precision = core.get_property("CPU", ov.properties.hint.inference_precision())
+log.info(f'\t\inference_precision: {inference_precision}')
+
+inputs = tokenizer(args.prompt, return_tensors="pt")
+prompt_tokens = inputs.input_ids.shape[1]
+perf = {"latency": []}
+
+log.info(" --- start generating --- ")
+st = time.perf_counter()
+output_ids = model.generate(inputs.input_ids,
+                                max_length=args.max_sequence_length+prompt_tokens,
+                                perf=perf)
+end = time.perf_counter()
+
+
+completion_ids = output_ids[0].tolist()[prompt_tokens:]
+
+log.info(" --- text decoding --- ")
+completion = tokenizer.decode(completion_ids,
+                                     skip_special_tokens=True,
+                                     clean_up_tokenization_spaces=False)
+log.info(f"Generation took {end - st:.3f} s on {args.device}")
+latency = perf["latency"]
+print("latency len: ", len(latency))
+result= {
+    "completion": completion,
+    "prompt_tokens": prompt_tokens,
+    "total_dur_s": end-st,  # total time, include tokeninzer.encode+decode, tokens generation
+    "completion_tokens": len(completion_ids),
+    # total tokens completion latency, except tokenizer.decode time
+    "total_token_latency_s": sum(latency),
+    # first token completion latency
+    "first_token_latency_ms": latency[0]*1000 if len(latency) > 0 else 0,
+    # next token completion latency
+    "next_token_latency_ms": sum(latency[1:])*1000 / len(latency[1:]) if len(latency) > 1 else 0,
+    # average token completion latency
+    "avg_token_latency_ms": sum(latency)*1000 / len(latency) if len(latency) > 0 else 0,
+}
+log.info(result)
---
+++ llama2-openvino/gen_ov.py
@@ -0,0 +1,225 @@
+from transformers import LlamaTokenizer
+import openvino as ov
+from openvino.runtime import Core, Tensor
+from pathlib import Path
+import numpy as np
+import argparse
+import time
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+    summation = e_x.sum(axis=-1, keepdims=True)
+    return e_x / summation
+
+
+def process_logits(cur_length, scores, eos_token_id, min_length=0):
+    if cur_length < min_length:
+        scores[:, eos_token_id] = -float("inf")
+    return scores
+
+
+def get_top_k_logits(scores, top_k):
+    filter_value = -float("inf")
+    top_k = min(max(top_k, 1), scores.shape[-1])
+    top_k_scores = -np.sort(-scores)[:, :top_k]
+    indices_to_remove = scores < np.min(top_k_scores)
+    filtred_scores = np.ma.array(scores,
+                                 mask=indices_to_remove,
+                                 fill_value=filter_value).filled()
+    return filtred_scores
+
+
+def generate_sequence(sampling, input_ids, attention_mask, eos_token_id,
+                      max_sequence_length, perf):
+    latency = perf["latency"]
+    st = time.perf_counter()
+
+    past_key_values = None
+    prompt_len = len(input_ids[0])
+    count = 0
+    
+    while True:
+        try:
+            inputs = {}
+            if past_key_values is not None:
+                inputs = dict(zip(key_value_input_names, past_key_values))
+                inputs["input_ids"] = input_ids[:, -1:]
+                cur_input_len = 1
+            else:
+                inputs["input_ids"] = input_ids
+                shape_input_ids = input_ids.shape
+                num_attention_heads = 1
+                for input_name in key_value_input_names:
+                    model_inputs = model.input(input_name)
+                    shape = model_inputs.get_partial_shape()
+                    shape[0] = shape_input_ids[0] * num_attention_heads
+                    if shape[2].is_dynamic:
+                        shape[2] = 0
+                    if shape[1].is_dynamic:
+                        shape[1] = 0
+                    inputs[input_name] = Tensor(model_inputs.get_element_type(),
+                                                shape.get_shape())
+            cur_input_len = len(inputs["input_ids"][0])
+            if "attention_mask" in input_names and attention_mask is not None:
+                inputs["attention_mask"] = attention_mask
+            request.start_async(inputs, shared_memory=True)
+            request.wait()
+            count += 1
+            logits = request.get_tensor("logits").data
+            past_key_values = tuple(
+                request.get_tensor(key).data for key in key_value_output_names)
+            next_token_logits = logits[:, cur_input_len - 1, :]
+            # pre-process distribution
+            next_token_scores = process_logits(len(input_ids[0]),
+                                            next_token_logits, eos_token_id)
+            top_k = 20
+            next_token_scores = get_top_k_logits(next_token_scores, top_k)
+            # get next token id
+            if sampling:
+                probs = softmax(next_token_scores)
+                next_tokens = np.random.choice(probs.shape[-1],
+                                            1,
+                                            p=probs[0],
+                                            replace=True)
+            else:
+                next_tokens = np.argmax(next_token_scores, axis=-1)
+            # break the loop if max length or end of text token is reached
+            if (len(input_ids[0]) - prompt_len
+                ) == max_sequence_length or next_tokens == eos_token_id:
+                # end = time.perf_counter()
+                # latency.append(end - st)
+                break
+            else:
+                input_ids = np.concatenate((input_ids, [next_tokens]), axis=-1)
+                attention_mask = np.concatenate(
+                    (attention_mask, [[1] * len(next_tokens)]), axis=-1)
+                
+        finally:
+            end = time.perf_counter()
+            latency.append(end - st)
+            st = end
+            tmp = input_ids[0].tolist()
+            print(len(latency), len(tmp), tmp)
+    return input_ids, count
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('-h',
+                        '--help',
+                        action='help',
+                        help='Show this help message and exit.')
+    parser.add_argument('-m',
+                        '--model_path',
+                        required=True,
+                        type=str,
+                        help='Required. path of IR model and tokenizer')
+    parser.add_argument('-p',
+                        '--prompt',
+                        type=str,
+                        default="What is AI?",
+                        help='Required. prompt sentence')
+    parser.add_argument('-l',
+                        '--max_sequence_length',
+                        default=128,
+                        required=False,
+                        type=int,
+                        help='maximun lengh of output')
+    parser.add_argument('-d',
+                        '--device',
+                        default='CPU',
+                        required=False,
+                        type=str,
+                        help='device for inference')
+    parser.add_argument('-s',
+                        '--sampling',
+                        default=False,
+                        required=False,
+                        type=bool,
+                        help='sampling or not, use greedy search for bench, random sampling for chat')
+    args = parser.parse_args()
+
+    num_pkv = 2
+    core = Core()
+    ir_model_path = Path(args.model_path)
+    ir_model = ir_model_path / "openvino_model.xml"
+    
+    print(" --- load tokenizer --- ")
+    tokenizer = LlamaTokenizer.from_pretrained(args.model_path)
+    inputs = tokenizer(args.prompt, return_tensors="np")
+    
+    print(" --- reading model --- ")
+    # read the model and corresponding weights from file
+    model = core.read_model(ir_model)
+    input_names = {
+        key.get_any_name(): idx
+        for idx, key in enumerate(model.inputs)
+    }
+    output_names = {
+        key.get_any_name(): idx
+        for idx, key in enumerate(model.outputs)
+    }
+    key_value_input_names = [key for key in input_names if "key_values" in key]
+    key_value_output_names = [key for key in output_names if "present" in key]
+
+    print(" --- model compiling --- ")
+    # compile the model for CPU devices
+    request = core.compile_model(
+        model=model, device_name=args.device).create_infer_request()
+
+    # get inference precision
+    inference_precision = core.get_property("CPU", ov.properties.hint.inference_precision())
+    print(f'\t\inference_precision: {inference_precision}')
+
+    perf = {"latency": []}
+
+    print(" --- start generating --- ")
+    st = time.perf_counter()
+    output_ids, num_tokens = generate_sequence(
+        args.sampling,
+        inputs["input_ids"],
+        inputs["attention_mask"],
+        eos_token_id=tokenizer.eos_token_id,
+        max_sequence_length=args.max_sequence_length,
+        perf=perf,
+    )
+    end = time.perf_counter()
+    
+    latency = perf["latency"]
+    print(f'latency len: {len(latency)}')
+    
+
+    output_text = " "
+    # Convert IDs to words and make the sentence from it
+    
+    print(" --- text decoding --- ")
+    # output_text = tokenizer.batch_decode(output_ids,
+    #                                      skip_special_tokens=True,
+    #                                      clean_up_tokenization_spaces=False)[0]
+    print("output_ids[0] len: ", len(output_ids[0]))
+    prompt_tokens = inputs.input_ids.shape[1]
+    completion_ids = output_ids[0].tolist()[prompt_tokens:]
+    completion = tokenizer.decode(completion_ids,
+                                     skip_special_tokens=True,
+                                     clean_up_tokenization_spaces=False)
+    print(
+        f"Generated {num_tokens} tokens in {end - st:.3f} s on {args.device}"
+    )
+    # print(f"Response: {output_text}")
+
+    
+    result= {
+    "completion": completion,
+    "prompt_tokens": prompt_tokens,
+    "total_dur_s": end-st,  # total time, include tokeninzer.encode+decode, tokens generation
+    "completion_tokens": len(completion_ids),
+    # total tokens completion latency, except tokenizer.decode time
+    "total_token_latency_s": sum(latency),
+    # first token completion latency
+    "first_token_latency_ms": latency[0]*1000 if len(latency) > 0 else 0,
+    # next token completion latency
+    "next_token_latency_ms": sum(latency[1:])*1000 / len(latency[1:]) if len(latency) > 1 else 0,
+    # average token completion latency
+    "avg_token_latency_ms": sum(latency)*1000 / len(latency) if len(latency) > 0 else 0,
+    }
+    print(result)
